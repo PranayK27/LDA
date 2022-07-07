503ae2df7d2ac3f5e6ddbe3dc5292c2addf16c35
Pranay Kekre <kekrepranay@gmail.com>
2022-07-07 11:43:53 +0100

    modules

--- apps/lda/src/app/app.module.ts
+++ apps/lda/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import {MyCounterModule} from "@pranay/my-counter";
 import {BooksModule} from "@pranay/books";
 import {MatTabsModule} from '@angular/material/tabs';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, MyCounterModule, BooksModule, MatTabsModule],
+  imports: [BrowserModule, MyCounterModule, BooksModule, MatTabsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
