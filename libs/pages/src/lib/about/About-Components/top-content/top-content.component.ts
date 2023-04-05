abfa2c1e4878e330883b825cba90e85de0b2b48e
PranayK27 <kekre.pranay@gmail.com>
2023-04-05 11:08:17 +0100

    responsiveness

--- libs/pages/src/lib/about/About-Components/top-content/top-content.component.ts
+++ libs/pages/src/lib/about/About-Components/top-content/top-content.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopContentComponent implements OnInit {
 
-  imagePath='libs/pages/src/lib/assets/images/innerpage/about-us.jpg';
+  imagePath='apps/lda/src/assets/images/innerpage/about-us.jpg';
 
   constructor() { }
 
