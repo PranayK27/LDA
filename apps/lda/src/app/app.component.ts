4e757b595dacf0b24b6d2e19361218f8858cfed5
PranayK27 <kekre.pranay@gmail.com>
2023-12-09 11:53:22 +0000

    minore change to finish enahance ui story

--- apps/lda/src/app/app.component.ts
+++ apps/lda/src/app/app.component.ts
@@ -1,25 +1,12 @@
 import {Component} from '@angular/core';
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
-
-
 @Component({
   selector: 'lda-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
+ // root component
 
   title = "LDA";
 }
