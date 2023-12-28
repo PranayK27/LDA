d674c0d11485cfe2a3c41978568b50e964d68e42
PranayK27 <kekre.pranay@gmail.com>
2023-12-28 16:28:58 +0000

    route sorted

--- apps/lda-e2e/src/mock/sources-data.ts
+++ apps/lda-e2e/src/mock/sources-data.ts
@@ -3,11 +3,11 @@ import { Sources } from '../../../../libs/pages/src/lib/blog/source-type';
 export const sourcesData: Sources[] = [
   {
     downloadLocation: 'https://github.com/PranayK27',
-    home: '/tech',
-    blog: '/tech/blog',
-    about: '/tech/about',
-    login: '/tech/login',
-    register: '/tech/register',
-    list: '/tech/list',
+    home: '/#/tech',
+    blog: '/#/tech/blog',
+    about: '/#/tech/about',
+    login: '/#/tech/login',
+    register: '/#/tech/register',
+    list: '/#/tech/list',
   }
 ];
