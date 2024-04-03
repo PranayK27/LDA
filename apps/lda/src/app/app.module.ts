577ab52ffb7a87478b53d2e43eac0e54d20e8d03
PranayK27 <kekre.pranay@gmail.com>
2024-04-03 19:17:37 +0100

    removed storybook

--- apps/lda/src/app/app.module.ts
+++ apps/lda/src/app/app.module.ts
@@ -15,19 +15,21 @@ import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatTableModule} from "@angular/material/table";
 import {MatPaginatorModule} from "@angular/material/paginator";
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import {TechnologiesModule} from "@lda/technologies";
 import {StoreModule} from "@ngrx/store";
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import {counterReducer} from "../../../../libs/my-counter/src/lib/counter.reducer";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
-import {
-  MultipleClassSelectorComponent
-} from "@storybook/angular/template/stories/basics/component-with-complex-selectors/multiple-selector.component";
+// import {
+//   MultipleClassSelectorComponent
+// } from "@storybook/angular/template/stories/basics/component-with-complex-selectors/multiple-selector.component";
 import { SimpleNotificationsModule } from 'angular2-notifications';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultipleClassSelectorComponent
+    // MultipleClassSelectorComponent
   ],
   imports: [
     StoreModule.forRoot(counterReducer),
