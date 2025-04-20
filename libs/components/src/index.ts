c50f10833c1da6c534381b645451cbd1d887aa92
PranayK27 <kekre.pranay@gmail.com>
2025-04-20 12:45:55 +0100

    deleted cache

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -2,3 +2,6 @@ export * from './lib/components.module';
 export * from './button/button.component';
 export * from './form-control/form-control.module';
 export * from './pages/page.component';
+export * from './button/button.component';
+export * from './form-control/form-control.module';
+export * from './pages/page.component';
--- libs/pages/src/index.ts
+++ libs/pages/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/pages.module';
 export * from './lib/shared/banner/banner.component';
 export * from './lib/shared/footer/footer.component';
+export * from './lib/shared/banner/banner.component';
+export * from './lib/shared/footer/footer.component';
